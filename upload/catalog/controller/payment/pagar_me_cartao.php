f324afa19d1d7659337b1fea3c9a62a7d209de69
Rodrigo Santiago <rodrigo@santive.com>
2015-01-02 20:10:20 -0200

    Envio de transacao de cartao de credito para o Pagar.me

---
+++ upload/catalog/controller/payment/pagar_me_cartao.php
@@ -0,0 +1,166 @@
+<?php
+
+require_once DIR_SYSTEM . 'library/PagarMe/Pagarme.php';
+
+class ControllerPaymentPagarMeCartao extends Controller {
+
+    protected function index() {
+
+        $this->language->load('payment/pagar_me_cartao');
+        $this->load->model('checkout/order');
+        $this->load->model('account/customer');
+        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+        $customer = $this->model_account_customer->getCustomer($order_info['customer_id']);
+
+        if ($this->customer->isLogged()) {
+            $this->data['nome_cartao'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
+        }
+
+        $this->data['total'] = str_replace(".", "", number_format($order_info['total'], 2, ".", ""));
+
+        $this->data['button_confirm'] = $this->language->get('button_confirm');
+        $this->data['text_information'] = $this->language->get('text_information');
+        $this->data['text_wait'] = $this->language->get('text_wait');
+        $this->data['text_information'] = $this->config->get('pagar_me_cartao_text_information');
+        $this->data['url'] = $this->url->link('payment/pagar_me_cartao/confirm', '', 'SSL');
+        $this->data['url2'] = $this->url->link('payment/pagar_me_cartao/error', '', 'SSL');
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/pagar_me_cartao.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/payment/pagar_me_cartao.tpl';
+        } else {
+            $this->template = 'default/template/payment/pagar_me_cartao.tpl';
+        }
+        // incluindo css
+        if (file_exists('catalog/view/theme/' . $this->config->get('config_template') . '/stylesheet/pagar_me_cartao.css')) {
+            $this->data['stylesheet'] = 'catalog/view/theme/' . $this->config->get('config_template') . '/stylesheet/pagar_me_cartao.css';
+        } else {
+            $this->data['stylesheet'] = 'catalog/view/theme/default/stylesheet/pagar_me_cartao.css';
+        }
+
+        $this->render();
+    }
+
+    public function confirm() {
+
+
+        $this->load->model('checkout/order');
+
+        $this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('pagar_me_cartao_order_paid'));
+
+        $this->redirect($this->url->link('checkout/success'));
+    }
+
+    public function error() {
+
+
+        $this->load->model('checkout/order');
+
+        $this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('pagar_me_cartao_order_refused'));
+
+        if (isset($this->session->data['order_id'])) {
+            $this->cart->clear();
+
+            unset($this->session->data['shipping_method']);
+            unset($this->session->data['shipping_methods']);
+            unset($this->session->data['payment_method']);
+            unset($this->session->data['payment_methods']);
+            unset($this->session->data['guest']);
+            unset($this->session->data['comment']);
+            unset($this->session->data['order_id']);
+            unset($this->session->data['coupon']);
+            unset($this->session->data['reward']);
+            unset($this->session->data['voucher']);
+            unset($this->session->data['vouchers']);
+        }
+
+        $this->language->load('payment/pagar_me_cartao');
+
+        $this->document->setTitle($this->language->get('heading_title'));
+
+        $this->data['breadcrumbs'] = array();
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('common/home'),
+            'text' => $this->language->get('text_home'),
+            'separator' => false
+        );
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('checkout/cart'),
+            'text' => $this->language->get('text_basket'),
+            'separator' => $this->language->get('text_separator')
+        );
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('checkout/checkout', '', 'SSL'),
+            'text' => $this->language->get('text_checkout'),
+            'separator' => $this->language->get('text_separator')
+        );
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('payment/cielo_message'),
+            'text' => $this->language->get('text_no_success'),
+            'separator' => $this->language->get('text_separator')
+        );
+
+        $this->data['heading_title'] = $this->language->get('heading_title');
+
+        if ($this->customer->isLogged()) {
+            $this->data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/order', '', 'SSL'), $this->url->link('information/contact'));
+        } else {
+            $this->data['text_message'] = sprintf($this->language->get('text_guest'), $this->url->link('information/contact'));
+        }
+
+        $this->data['button_continue'] = $this->language->get('button_continue');
+
+        $this->data['continue'] = $this->url->link('common/home');
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/pagar_me_cartao_message.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/payment/pagar_me_cartao_message.tpl';
+        } else {
+            $this->template = 'default/template/payment/pagar_me_cartao_message.tpl';
+        }
+
+        $this->children = array(
+            'common/column_left',
+            'common/column_right',
+            'common/content_top',
+            'common/content_bottom',
+            'common/footer',
+            'common/header'
+        );
+
+        $this->response->setOutput($this->render());
+    }
+
+    public function callback() {
+
+    }
+
+    public function payment() {
+
+        Pagarme::setApiKey($this->config->get('pagar_me_cartao_api'));
+
+        $transaction = new PagarMe_Transaction(array(
+            'amount' => $_POST['amount'],
+            'card_hash' => $_POST['card_hash']
+        ));
+
+        $transaction->charge();
+
+        $status = $transaction->status; // status da transação
+
+        $json = array();
+
+        if ($status == 'paid') {
+            $json['success'] = true;
+        } else {
+            $json['success'] = false;
+        }
+
+        $this->response->setOutput(json_encode($json));
+    }
+
+}
+
+?>
---
+++ upload/catalog/view/theme/default/template/payment/pagar_me_cartao.tpl
@@ -0,0 +1,133 @@
+<link rel="stylesheet" href="<?php echo $stylesheet; ?>">
+<?php if ($text_information) { ?>
+    <div class="payment-information"><?php echo $text_information; ?></div>
+<?php } ?>
+
+<div class="dados_cartao">
+
+    <form id="payment_form" method="POST">
+
+        <!-- Total do pedido -->
+        <input type="hidden" name="totalValue" id="totalValue" value="<?php echo $total; ?>" >
+
+        <div class="input-block-float">
+            <label for="card_number">Número do cartão</label>
+            <input type="text" id="card_number"/>
+        </div>
+
+        <div class="input-block-float">
+            <!-- aqui colocar um tooltip com uma imagem mostrando onde fica o CVV de um cartão de crédito -->
+
+            <label for="card_cvv" id="label-cvc">CVV <span id="tool-tip-cvc"><i class="fa fa-question-circle"></i> <span id="tool-tip-content"><img src="catalog/view/theme/default/image/bancos/cartao-cvc.png" alt=""></span></span></label>
+            <input type="text" id="card_cvv" size="4" placeholder="CVV" maxlength="4" class="so_numeros" />
+
+        </div>
+        <div class="cf"></div>
+        <div class="input-block">
+            <label>Validade do cartão</label>
+            <input type="text" id="card_expiration_month" size="2" maxlength="2" placeholder="MM" class="so_numeros"/>
+            <input type="text" id="card_expiration_year" size="4" placeholder="AAAA" maxlength="4" class="so_numeros" />
+        </div>
+
+        <div class="input-block">
+            <label for="card_holder_name">Nome impresso no cartão</label>
+            <input type="text" id="card_holder_name" value="<?php echo $nome_cartao ?>" />
+        </div>
+
+    </form>
+</div>
+
+<div class="buttons">
+    <div class="right"><a id="button-confirm" class="button disabled"><span><?php echo $button_confirm; ?></span></a><span id="aguardando">Aguarde...</span></div>
+</div>
+<style>
+    #aguardando{
+        display: none;
+    }
+
+    #installmentsWrapper{
+        display: none;
+    }
+</style>
+
+<?php if (!$this->config->get('dados_status')): ?>
+    <script type="text/javascript" src="catalog/view/javascript/mask.js"></script>
+<?php endif; ?>
+
+<script type="text/javascript"><!--
+    /* Máscaras dos inputs do cartão */
+    $("#card_number").livequery(function () {
+        $(this).mask("9999999999999999");
+    });
+    $("#card_cvv").livequery(function () {
+        $(this).mask("999?9");
+    });
+    $("#card_expiration_month").livequery(function () {
+        $(this).mask("99");
+    });
+    $("#card_expiration_year").livequery(function () {
+        $(this).mask("9999");
+    });
+
+    $('#button-confirm').bind('click', function () {
+
+        /*Função Pagar.me*/
+        PagarMe.encryption_key = "<?php echo $this->config->get('pagar_me_cartao_criptografia'); ?>";
+
+        var form = $("#payment_form");
+
+        //form.submit(function (event) { // quando o form for enviado...
+        // inicializa um objeto de cartão de crédito e completa
+        // com os dados do form
+        var creditCard = new PagarMe.creditCard();
+        creditCard.cardHolderName = $("#payment_form #card_holder_name").val();
+        creditCard.cardExpirationMonth = $("#payment_form #card_expiration_month").val();
+        creditCard.cardExpirationYear = $("#payment_form #card_expiration_year").val();
+        creditCard.cardNumber = $("#payment_form #card_number").val();
+        creditCard.cardCVV = $("#payment_form #card_cvv").val();
+
+        // pega os erros de validação nos campos do form
+        var fieldErrors = creditCard.fieldErrors();
+
+        //Verifica se há erros
+        var hasErrors = false;
+        for (var field in fieldErrors) {
+            hasErrors = true;
+            break;
+        }
+
+        if (hasErrors) {
+            // realiza o tratamento de errors
+            alert("Verifique se os dados informados estão corretos. Qualque rproblema entre em contato com a loja.");
+            $('#button-confirm').show();
+        } else {
+            //console.log("oi")
+            // se não há erros, gera o card_hash...
+            creditCard.generateHash(function (cardHash) {
+                // ...coloca-o no form...
+                form.append($('<input type="hidden" id="card_hash" name="card_hash">').val(cardHash));
+                // e envia o form
+                $.ajax({
+                type: 'POST',
+                url: 'index.php?route=payment/pagar_me_cartao/payment',
+                dataType: 'json',
+                data: { amount: $("#totalValue").val(), card_hash: $("#card_hash").val() },
+                success: function (response) {
+                    if (response['error']) {
+                        alert('Ocorreu um erro inesperado. Por favor contate a loja.')
+                    } else if (response['success']) {
+                        // alert(response['success']);
+                        //$.colorbox({href: response['success']});
+                        //window.open(response['success']);
+                        location = '<?php echo $url; ?>';
+                    } else {
+                        location = '<?php echo $url2; ?>';
+                    }
+                }
+            });
+            });
+        }
+    });
+//--></script>
+</script>
+
