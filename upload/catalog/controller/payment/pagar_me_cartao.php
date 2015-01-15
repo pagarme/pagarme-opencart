ef946602b06df22d78087ba5b58b15719e5818fc
Rodrigo Santiago <rodrigo@santive.com>
2015-01-15 19:12:58 -0200

    Cartão com status inicial processando

--- upload/catalog/controller/payment/pagar_me_cartao.php
+++ upload/catalog/controller/payment/pagar_me_cartao.php
@@ -66,7 +66,7 @@ class ControllerPaymentPagarMeCartao extends Controller {
 
         $this->load->model('checkout/order');
 
-        $this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('pagar_me_cartao_order_paid'));
+        $this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('pagar_me_cartao_order_processing'));
 
         $this->redirect($this->url->link('checkout/success'));
     }
