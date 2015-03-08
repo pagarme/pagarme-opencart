747308ba199ce22a638cca25a740d6e4d6c691eb
Rodrigo Santiago <rodrigo@santive.com>
2015-03-08 12:22:46 -0300

    Correçao do texto de impressao de boleto que vai no e-mail de confirmaçao do pedido Imagem do Pagar.Me no admin movida para o local correto Inserçao da URL do boleto na tabela do pedido

--- upload/catalog/controller/payment/pagar_me_boleto.php
+++ upload/catalog/controller/payment/pagar_me_boleto.php
@@ -43,7 +43,7 @@ class ControllerPaymentPagarMeBoleto extends Controller {
 
         $order = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
-        $this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('pagar_me_boleto_order_waiting_payment'), 'Imprima seu bolero aqui -> ' . $order['pagar_me_boleto_url']);
+        $this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('pagar_me_boleto_order_waiting_payment'), 'Imprima seu boleto aqui -> ' . $order['pagar_me_boleto_url']);
 
         $this->redirect($this->url->link('checkout/success'));
     }
