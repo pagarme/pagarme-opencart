7e74448c7632c24101cc4ae60aca38921e00f5b2
Rodrigo Santiago <rodrigo@santive.com>
2015-11-19 16:06:55 -0200

    Remoção dos arquivos de IDE do repositório

---
+++ Checkout Pagar.Me/1.5.x/upload/admin/controller/payment/pagar_me_checkout.php
@@ -0,0 +1,305 @@
+<?php
+
+class ControllerPaymentPagarMeCheckout extends Controller {
+
+    private $error = array();
+
+    public function index() {
+        $this->load->language('payment/pagar_me_checkout');
+
+        $this->document->setTitle($this->language->get('heading_title'));
+
+        $this->document->addScript('view/javascript/pagar_me_checkout/colorpicker/js/colorpicker.js');
+
+        $this->document->addStyle('view/javascript/pagar_me_checkout/colorpicker/css/colorpicker.css');
+
+        $this->load->model('setting/setting');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && ($this->validate())) {
+            $this->model_setting_setting->editSetting('pagar_me_checkout', $this->request->post);
+
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            $this->redirect($this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'));
+        }
+        $this->data['heading_title'] = $this->language->get('heading_title');
+
+        $this->data['text_enabled'] = $this->language->get('text_enabled');
+        $this->data['text_disabled'] = $this->language->get('text_disabled');
+        $this->data['text_all_zones'] = $this->language->get('text_all_zones');
+        $this->data['text_none'] = $this->language->get('text_none');
+        $this->data['text_yes'] = $this->language->get('text_yes');
+        $this->data['text_no'] = $this->language->get('text_no');
+
+        $this->data['entry_criptografia'] = $this->language->get('entry_criptografia');
+        $this->data['entry_api'] = $this->language->get('entry_api');
+        $this->data['entry_nome'] = $this->language->get('entry_nome');
+        $this->data['entry_texto_botao'] = $this->language->get('entry_texto_botao');
+        $this->data['entry_payment_methods'] =  $this->language->get('entry_payment_methods');
+        $this->data['entry_card_brands'] =      $this->language->get('entry_card_brands');
+        $this->data['entry_max_installments'] =  $this->language->get('entry_max_installments');
+        $this->data['entry_interest_rate'] =  $this->language->get('entry_insterest_rate');
+        $this->data['entry_ui_color'] =         $this->language->get('entry_ui_color');
+        $this->data['entry_button_css_class'] =         $this->language->get('entry_button_css_class');
+        $this->data['entry_text_information'] = $this->language->get('entry_text_information');
+        $this->data['entry_order_status'] = $this->language->get('entry_order_status');
+        $this->data['entry_order_waiting_payment'] = $this->language->get('entry_order_waiting_payment');
+        $this->data['entry_order_paid'] = $this->language->get('entry_order_paid');
+        $this->data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
+        $this->data['entry_status'] = $this->language->get('entry_status');
+        $this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
+        $this->data['entry_update_status_alert'] = $this->language->get('entry_update_status_alert');
+        $this->data['entry_total'] = $this->language->get('entry_total');
+
+        $this->data['button_save'] = $this->language->get('button_save');
+        $this->data['button_cancel'] = $this->language->get('button_cancel');
+
+        if (isset($this->error['warning'])) {
+            $this->data['error_warning'] = $this->error['warning'];
+        } else {
+            $this->data['error_warning'] = '';
+        }
+
+        if (isset($this->error['criptografia'])) {
+            $this->data['error_criptografia'] = $this->error['criptografia'];
+        } else {
+            $this->data['error_criptografia'] = '';
+        }
+
+        if (isset($this->error['texto_botao'])) {
+            $this->data['error_texto_botao'] = $this->error['texto_botao'];
+        } else {
+            $this->data['error_texto_botao'] = '';
+        }
+
+        if (isset($this->error['payment_methods'])) {
+            $this->data['error_payment_methods'] = $this->error['payment_methods'];
+        } else {
+            $this->data['error_payment_methods'] = '';
+        }
+
+        if (isset($this->error['card_brands'])) {
+            $this->data['error_card_brands'] = $this->error['card_brands'];
+        } else {
+            $this->data['error_card_brands'] = '';
+        }
+
+        if (isset($this->error['max_installments'])) {
+            $this->data['error_max_installments'] = $this->error['max_installments'];
+        } else {
+            $this->data['error_max_installments'] = '';
+        }
+
+        if (isset($this->error['api'])) {
+            $this->data['error_api'] = $this->error['api'];
+        } else {
+            $this->data['error_api'] = '';
+        }
+
+        if (isset($this->error['nome'])) {
+            $this->data['error_nome'] = $this->error['nome'];
+        } else {
+            $this->data['error_nome'] = '';
+        }
+
+        $this->data['breadcrumbs'] = array();
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+            'text' => $this->language->get('text_home'),
+            'separator' => false
+        );
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'),
+            'text' => $this->language->get('text_payment'),
+            'separator' => ' :: '
+        );
+
+        $this->data['breadcrumbs'][] = array(
+            'href' => $this->url->link('payment/pagar_me_checkout', 'token=' . $this->session->data['token'], 'SSL'),
+            'text' => $this->language->get('heading_title'),
+            'separator' => ' :: '
+        );
+
+        $this->data['action'] = $this->url->link('payment/pagar_me_checkout', 'token=' . $this->session->data['token'], 'SSL');
+
+        $this->data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
+
+        if (isset($this->request->post['pagar_me_checkout_criptografia'])) {
+            $this->data['pagar_me_checkout_criptografia'] = $this->request->post['pagar_me_checkout_criptografia'];
+        } else {
+            $this->data['pagar_me_checkout_criptografia'] = $this->config->get('pagar_me_checkout_criptografia');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_texto_botao'])) {
+            $this->data['pagar_me_checkout_texto_botao'] = $this->request->post['pagar_me_checkout_texto_botao'];
+        } else {
+            $this->data['pagar_me_checkout_texto_botao'] = $this->config->get('pagar_me_checkout_texto_botao');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_payment_methods'])) {
+            $this->data['pagar_me_checkout_payment_methods'] = $this->request->post['pagar_me_checkout_payment_methods'];
+        } elseif($this->config->get('pagar_me_checkout_payment_methods')) {
+            $this->data['pagar_me_checkout_payment_methods'] = $this->config->get('pagar_me_checkout_payment_methods');
+        }else{
+            $this->data['pagar_me_checkout_payment_methods'] =  array();
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_card_brands'])) {
+            $this->data['pagar_me_checkout_card_brands'] = $this->request->post['pagar_me_checkout_card_brands'];
+        } elseif($this->config->get('pagar_me_checkout_card_brands')) {
+            $this->data['pagar_me_checkout_card_brands'] = $this->config->get('pagar_me_checkout_card_brands');
+        }else{
+            $this->data['pagar_me_checkout_card_brands'] = array();
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_max_installments'])) {
+            $this->data['pagar_me_checkout_max_installments'] = $this->request->post['pagar_me_checkout_max_installments'];
+        } else {
+            $this->data['pagar_me_checkout_max_installments'] = $this->config->get('pagar_me_checkout_max_installments');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_interest_rate'])) {
+            $this->data['pagar_me_checkout_interest_rate'] = $this->request->post['pagar_me_checkout_interest_rate'];
+        } else {
+            $this->data['pagar_me_checkout_interest_rate'] = $this->config->get('pagar_me_checkout_interest_rate');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_ui_color'])) {
+            $this->data['pagar_me_checkout_ui_color'] = $this->request->post['pagar_me_checkout_ui_color'];
+        } else {
+            $this->data['pagar_me_checkout_ui_color'] = $this->config->get('pagar_me_checkout_ui_color');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_button_css_class'])) {
+            $this->data['pagar_me_checkout_button_css_class'] = $this->request->post['pagar_me_checkout_button_css_class'];
+        } else {
+            $this->data['pagar_me_checkout_button_css_class'] = $this->config->get('pagar_me_checkout_button_css_class');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_api'])) {
+            $this->data['pagar_me_checkout_api'] = $this->request->post['pagar_me_checkout_api'];
+        } else {
+            $this->data['pagar_me_checkout_api'] = $this->config->get('pagar_me_checkout_api');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_nome'])) {
+            $this->data['pagar_me_checkout_nome'] = $this->request->post['pagar_me_checkout_nome'];
+        } else {
+            $this->data['pagar_me_checkout_nome'] = $this->config->get('pagar_me_checkout_nome');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_text_information'])) {
+            $this->data['pagar_me_checkout_text_information'] = $this->request->post['pagar_me_checkout_text_information'];
+        } else {
+            $this->data['pagar_me_checkout_text_information'] = $this->config->get('pagar_me_checkout_text_information');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_order_waiting_payment'])) {
+            $this->data['pagar_me_checkout_order_waiting_payment'] = $this->request->post['pagar_me_checkout_order_waiting_payment'];
+        } else {
+            $this->data['pagar_me_checkout_order_waiting_payment'] = $this->config->get('pagar_me_checkout_order_waiting_payment');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_order_paid'])) {
+            $this->data['pagar_me_checkout_order_paid'] = $this->request->post['pagar_me_checkout_order_paid'];
+        } else {
+            $this->data['pagar_me_checkout_order_paid'] = $this->config->get('pagar_me_checkout_order_paid');
+        }
+
+        $this->load->model('localisation/order_status');
+
+        $this->data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+
+        if (isset($this->request->post['pagar_me_checkout_geo_zone_id'])) {
+            $this->data['pagar_me_checkout_geo_zone_id'] = $this->request->post['pagar_me_checkout_geo_zone_id'];
+        } else {
+            $this->data['pagar_me_checkout_geo_zone_id'] = $this->config->get('pagar_me_checkout_geo_zone_id');
+        }
+
+        $this->load->model('localisation/geo_zone');
+
+        $this->data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+
+        if (isset($this->request->post['pagar_me_checkout_status'])) {
+            $this->data['pagar_me_checkout_status'] = $this->request->post['pagar_me_checkout_status'];
+        } else {
+            $this->data['pagar_me_checkout_status'] = $this->config->get('pagar_me_checkout_status');
+        }
+
+        if (isset($this->request->post['pagar_me_checkout_sort_order'])) {
+            $this->data['pagar_me_checkout_sort_order'] = $this->request->post['pagar_me_checkout_sort_order'];
+        } else {
+            $this->data['pagar_me_checkout_sort_order'] = $this->config->get('pagar_me_checkout_sort_order');
+        }
+
+        $this->template = 'payment/pagar_me_checkout.tpl';
+        $this->children = array(
+            'common/header',
+            'common/footer'
+        );
+
+        $this->response->setOutput($this->render(TRUE), $this->config->get('config_compression'));
+    }
+
+    private function validate() {
+
+        if (!$this->user->hasPermission('modify', 'payment/pagar_me_checkout')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        if (!$this->request->post['pagar_me_checkout_criptografia']) {
+            $this->error['criptografia'] = $this->language->get('error_criptografia');
+        }
+
+        if (!$this->request->post['pagar_me_checkout_texto_botao']) {
+            $this->error['texto_botao'] = $this->language->get('error_texto_botao');
+        }
+
+        if (!$this->request->post['pagar_me_checkout_payment_methods']) {
+            $this->error['payment_methods'] = $this->language->get('error_payment_methods');
+        }
+        
+        if (!$this->request->post['pagar_me_checkout_card_brands']) {
+            $this->error['card_brands'] = $this->language->get('error_card_brands');
+        }
+
+        if (!$this->request->post['pagar_me_checkout_max_installments']) {
+            $this->error['max_installments'] = $this->language->get('error_max_installments');
+        }
+
+        if (!$this->request->post['pagar_me_checkout_api']) {
+            $this->error['api'] = $this->language->get('error_api');
+        }
+
+        if (!$this->request->post['pagar_me_checkout_nome']) {
+            $this->error['nome'] = $this->language->get('error_nome');
+        }
+
+//        var_dump($this->error); exit;
+
+        if (!$this->error) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function install() {
+        $this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "pagar_me_transaction` (
+  `pagar_me_transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `transaction_id` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`pagar_me_transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1");
+
+        $this->db->query("ALTER TABLE  `" . DB_PREFIX . "order` ADD `pagar_me_checkout_url` VARCHAR( 512 ) NULL DEFAULT NULL AFTER  `payment_code`");
+    }
+
+    public function uninstall() {
+    }
+
+}
+
+?>
---
+++ readme.md
@@ -0,0 +1,45 @@
+#########################################################
+##          Módulo integração Pagar.Me                 ##
+##          Desenvolvedor: Santive Tecnologia          ##
+##          Contato: contato@santive.com               ##
+##                                                     ##
+#########################################################
+
+### Compatibilidade
+
+ OpenCart 1.5.x, deve pegar também em Opencarts mais antigos
+
+### Funcionalidades
+
+ Integra sua loja virtual OpenCart com o gateway de pagamentos Pagar.Me http://www.pagar.me
+
+ Segunda via de boleto
+
+ Atualização de status automática
+ 
+ Configuração de parcelamento de cartão
+
+
+
+### Instalação e configuração
+## Versão 1.5.x
+1. Copie todo o conteúdo da pasta "upload" para a raíz da sua loja
+2. Acesse o painel administrativo da sua loja
+3. Acesse Extensões > Formas de pagamento e instale os módulos (Cartão de crédito e/ou boleto)
+4. Preencha as informações necessária (as chaves API e criptografia são encontradas no dashboard do Pagar.Me)
+5. Pronto! O módulo está funcionando
+
+## Versão 2.x
+1. Acesse Extensões > Instalador
+2. Faça o upload do arquivo OCMOD
+3. Acesse Extensões > Modificações
+4. Clique no botão atualizar
+5. Pronto! O módulo está funcionando
+
+### Requisitos
+
+- Vqmod necessário. Baixe a última versão em http://www.vqmod.com
+
+- Sua loja deve estar configurada para receber o CPF/CNPJ do seu cliente nas versões 1.5.x do OpenCart
+
+- Para as versões 2.x o cadastro é personalizável, basta criar os campos
