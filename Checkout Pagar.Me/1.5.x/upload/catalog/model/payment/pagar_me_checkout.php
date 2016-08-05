4499a339cb72eda9348fcb7d2ce4b350a50b674b
socialgroups <socialgroups@outlook.com>
2016-08-05 14:20:54 -0300

    HOTFIX

--- Checkout Pagar.Me/1.5.x/upload/catalog/model/payment/pagar_me_checkout.php
+++ Checkout Pagar.Me/1.5.x/upload/catalog/model/payment/pagar_me_checkout.php
@@ -59,19 +59,16 @@ class ModelPaymentPagarMeCheckout extends Model {
 
         $desconto = $this->config->get('pagar_me_checkout_boleto_discount_percentage');
 
-        /* Pega a order do sub-toal */
-//        $sub_total_order_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_total` WHERE order_id = '" . (int)$order_id . "' AND code = 'sub_total'");
-
         $total_order_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_total` WHERE order_id = '" . (int)$order_id . "' AND code = 'total'");
 
         $discount_order = $total_order_query->row['sort_order'] - 1;
 
-        $valor_desconto = $order['total'] * $desconto / 100;
+         $valor_desconto = $this->cart->getSubtotal() * $desconto / 100;
 
         $this->db->query("INSERT INTO " . DB_PREFIX . "order_total SET order_id = '" . (int)$order_id . "', code = 'pagar_me_checkout_desconto', title = 'Desconto do boleto (" . $this->db->escape($desconto) . "%)', text = '" . $this->db->escape($this->currency->format($valor_desconto*-1)) . "', `value` = '" . (float)$valor_desconto*-1 . "', sort_order = '" . $discount_order . "'");
 
         /* Atualiza total do pedido */
-        $valor_com_desconto = $order['total'] - $valor_desconto;
+        $valor_com_desconto = $this->cart->getSubtotal() - $valor_desconto;
 
         $this->db->query("UPDATE " . DB_PREFIX . "order_total SET text = '" . $this->db->escape($this->currency->format($valor_com_desconto)) . "', `value` = '" . (float)$valor_com_desconto . "' WHERE order_id = '" . (int)$order_id . "' AND code = 'total'");
 
